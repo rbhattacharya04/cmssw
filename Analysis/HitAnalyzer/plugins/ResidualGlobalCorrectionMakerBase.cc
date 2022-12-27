35e7b0cfa60476fdfa865102f43d15b3dc59b0d5
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-12-27 13:45:28 +0100

    use glued alignment only for out-of-plane DOFs (v711)

--- Analysis/HitAnalyzer/plugins/ResidualGlobalCorrectionMakerBase.cc
+++ Analysis/HitAnalyzer/plugins/ResidualGlobalCorrectionMakerBase.cc
@@ -377,8 +377,8 @@ ResidualGlobalCorrectionMakerBase::beginRun(edm::Run const& run, edm::EventSetup
 // //       const bool align2d = ispixel || isglued || isendcap;      
 //       const DetId parmdetid = isglued ? DetId(gluedid) : det->geographicalId();
       
-//       const bool align2d = ispixel || isendcap;
-      const bool align2d = ispixel || isendcap || (alignGlued_ && isglued);
+      const bool align2d = ispixel || isendcap;
+      // const bool align2d = ispixel || isendcap || (alignGlued_ && isglued);
 //       const bool align2d = true;
 //       const bool align2d = ispixel;
 //       const bool align2d = false;
@@ -398,15 +398,15 @@ ResidualGlobalCorrectionMakerBase::beginRun(edm::Run const& run, edm::EventSetup
 //         parmset.emplace(1, det->geographicalId());
 //       }
 
-      parmset.emplace(0, aligndetid);
+      parmset.emplace(0, det->geographicalId());
       parmset.emplace(2, aligndetid);
       parmset.emplace(3, aligndetid);
       parmset.emplace(4, aligndetid);
-      parmset.emplace(5, aligndetid);
+      parmset.emplace(5, det->geographicalId());
 
       if (align2d) {
         //local y alignment parameters only for pixels and wedge modules
-        parmset.emplace(1, aligndetid);
+        parmset.emplace(1, det->geographicalId());
       }
 
       // bfield and material parameters are associated to glued detids where applicable
@@ -800,9 +800,10 @@ ResidualGlobalCorrectionMakerBase::beginRun(edm::Run const& run, edm::EventSetup
       const DetId parmdetid = isglued ? DetId(gluedid) : det->geographicalId();
       const GeomDet* parmDet = isglued ? globalGeometry->idToDet(parmdetid) : det;
       
-      const DetId aligndetid = alignGlued_ ? parmdetid : det->geographicalId();
+      // const DetId aligndetid = alignGlued_ ? parmdetid : det->geographicalId();
 
-      const bool align2d = detidparms.count(std::make_pair(1, aligndetid));
+      const bool align2d = detidparms.count(std::make_pair(1, det->geographicalId()));
+      // const bool align2d = detidparms.count(std::make_pair(1, aligndetid));
       
       const Surface &surface = det->surface();
       
@@ -813,7 +814,7 @@ ResidualGlobalCorrectionMakerBase::beginRun(edm::Run const& run, edm::EventSetup
       if (isglued) {
         GloballyPositioned<double> surfaceGlued = surfaceToDouble(parmDet->surface());
 
-        if (alignGlued_) {
+        if (false && alignGlued_) {
           applyAlignment(surfaceGlued, parmdetid);
         }
 
@@ -912,7 +913,7 @@ ResidualGlobalCorrectionMakerBase::beginRun(edm::Run const& run, edm::EventSetup
       }
 
 
-      if (!isglued || !alignGlued_) {
+      if (false && !isglued || !alignGlued_) {
         applyAlignment(surfaceD, det->geographicalId());
       }
 
